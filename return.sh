a4da35bb80b37f503ed5450d47c736ddfe5f8c1c
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-05-10 11:55:40 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -1,4 +1,4 @@
 read -p "当地IP:" ip
-wget -N https://github.com/fscarmen/tools/raw/main/besttrace
+wget -nc https://github.com/fscarmen/tools/raw/main/besttrace
 chmod +x besttrace
 ./besttrace $ip -g cn
