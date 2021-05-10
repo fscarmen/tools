240586a059a974394025ec2f386f582d16236573
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-05-10 21:31:47 +0800

    Delete root.sh

--- root.sh
+++
@@ -1,6 +0,0 @@
-read -p "自定义ROOT密码:" password
-echo root:$password | sudo chpasswd root
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-sudo service sshd restart
-echo -e "\033[32m 请重新登陆，用户名：root ， 密码：$password \033[0m"
