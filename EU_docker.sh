880b7ffa5122776678f50a039266348a0c8784c8
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-06-10 11:46:39 +0800

    Update EU_docker.sh

--- EU_docker.sh
+++ EU_docker.sh
@@ -1,8 +1,10 @@
+i=1
 docker ps -a
 read -p "参考上面 docker 列表，填入需启动的项目名(最后一列 NAMES 下面的):" dockername
 docker start $dockername
 until [ $? -eq 0 ]  
   do
+    let i++
     docker start $dockername
   done
-echo -e "\033[32m 恭喜！你的项目 $dockername 已经启动成功。 \033[0m"
+echo -e "\033[32m 恭喜！你的项目 $dockername 经过$i次努力终于启动成功。 \033[0m"
