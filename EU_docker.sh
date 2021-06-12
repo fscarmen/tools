9ec11e5137b8e9efa7d625fe8d90d9d1b560c30d
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-06-12 20:34:27 +0800

    Update EU_docker.sh

--- EU_docker.sh
+++ EU_docker.sh
@@ -1,3 +1,5 @@
+startTime=`date +%Y%m%d-%H:%M:%S`
+startTime_s=`date +%s`
 i=1
 docker ps -a
 read -p "参考上面 docker 列表，填入需启动的项目名(最后一列 NAMES 下面的):" dockername
@@ -6,5 +8,8 @@ until [ $? -eq 0 ]
   do
     let i++
     docker start $dockername
-  done
-echo -e "\033[32m 恭喜！你的项目 $dockername 经过$i次努力终于启动成功。 \033[0m"
+done
+endTime=`date +%Y%m%d-%H:%M:%S`
+endTime_s=`date +%s`
+sumTime=$[ $endTime_s - $startTime_s ]
+echo -e "\033[32m 恭喜！你的项目 $dockername 经过$i次努力终于启动成功，$startTime ---> $endTime", "总耗时:$sumTime秒。\033[0m"
