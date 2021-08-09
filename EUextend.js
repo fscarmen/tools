879d4c9485ab2634b6759ea8c1ee14adfe723893
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-08-09 22:42:59 +0800

    Create EUextend.js

---
+++ EUextend.js
@@ -0,0 +1,23 @@
+// @grant nodejs
+console.log("⏳ 开始执行 EUextend.py")
+$exec('python3 https://raw.githubusercontent.com/fscarmen/tools/main/EUextend.py', {
+  cwd: 'script/Shell',
+  timeout: 0,
+  env: {
+    USERNAME: $store.get('EUserv_ID', 'string'),
+    PASSWORD: $store.get('EUserv_PW', 'string'),
+    SCKEY: $store.get('SCKEY'),
+    COOL_PUSH_SKEY: $store.get('COOL_PUSH_SKEY'),
+    COOL_PUSH_MODE: $store.get('COOL_PUSH_MODE'),
+    PUSH_PLUS_TOKEN: $store.get('PUSH_PLUS_TOKEN'),
+    TG_BOT_TOKEN: $store.get('TG_BOT_TOKEN'),
+    TG_USER_ID: $store.get('TG_USER_ID'),
+    TG_API_HOST: $store.get('TG_API_HOST'),
+    WECOMCHAN_DOMAIN: $store.get('WECOMCHAN_DOMAIN'),
+    WECOMCHAN_SEND_KEY: $store.get('WECOMCHAN_SEND_KEY'),
+    WECOMCHAN_TO_USER: $store.get('WECOMCHAN_TO_USER')
+  },
+  cb(data, error) {
+    error ? console.error(error) : console.log(data)
+  }
+})
