be22b5bfd0a94c6d9e17d72003e2ceb09a9c7c9f
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-08-28 08:17:42 +0800

    Update docker_port.sh

--- docker_port.sh
+++ docker_port.sh
@@ -3,10 +3,8 @@ read -p "参考上面 docker 列表，填入需改端口的项目名:" dockernam
 read -p "原来的端口:" port1
 read -p "变更后的端口:" port2
 
-docker stop qinglong
+docker stop $dockername
 systemctl stop docker
 cd /var/lib/docker/containers/$(docker ps -a | grep $dockername | awk '{print $1}')*
-sed -i 's/"HostPort":"$port1"/"HostPort":"$port2"/g' hostconfig.json
-systemctl start docker
-docker start $dockername
+sed -i "s/\"HostPort\"\:\"$port1\"\/\"HostPort\"\:\"$port2\"/g" hostconfig.json
 echo -e "\033[32m 恭喜！你的项目 $dockername 端口 $port1 已变更为 $port2 。\033[0m"
