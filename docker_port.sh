5c1f43bc07592d66ebca7175eb48979f926ec2eb
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-08-28 15:05:58 +0800

    Update docker_port.sh

--- docker_port.sh
+++ docker_port.sh
@@ -4,9 +4,9 @@ read -p "原来的端口:" port1
 read -p "变更后的端口:" port2
 
 docker stop $dockername
-sleep 5
+sleep 10
 systemctl stop docker
-sleep 5
+sleep 10
 sed -i 's/"HostPort":"'$port1'"/"HostPort":"'$port2'"/g' /var/lib/docker/containers/$(docker ps -a | grep $dockername | awk '{print $1}')*/hostconfig.json
 echo -e "\033[32m 恭喜！你的项目 $dockername 端口 $port1 已变更为 $port2 。\033[0m"
 systemctl start docker
