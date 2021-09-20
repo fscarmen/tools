60af99a88693d8bed57640123e0288e535e108cc
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-09-21 07:54:59 +0800

    Update EU_compose.sh

--- EU_compose.sh
+++ EU_compose.sh
@@ -1,3 +1,4 @@
+if [[ -e docker-compose.yml ]]; then
 timedatectl set-timezone Asia/Shanghai
 startTime=`date +%Y%m%d-%H:%M:%S`
 startTime_s=`date +%s`
@@ -11,4 +12,7 @@ done
 endTime=`date +%Y%m%d-%H:%M:%S`
 endTime_s=`date +%s`
 sumTime=$[ $endTime_s - $startTime_s ]
-echo -e "\033[32m 恭喜！你的项目经过$i次努力终于启动成功，$startTime ---> $endTime", "总耗时:$sumTime秒。\033[0m"
+echo -e "\033[32m\033[01m 恭喜！你的项目经过$i次努力终于启动成功，$startTime ---> $endTime", "总耗时:$sumTime秒。\033[0m"
+
+else
+echo -e "\033[31m\033[01m 请进入有 docker-compose.yml 文件的项目路径\033[0m"
