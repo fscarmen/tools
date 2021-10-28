4d31c84f472bd3674d981386ebc3ff55893301a7
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-10-28 18:43:20 +0800

    Update b.sh

--- b.sh
+++ b.sh
@@ -121,16 +121,16 @@ status(){
 	clear
 	yellow "本项目专为 VPS 添加 wgcf 网络接口，详细说明：[https://github.com/fscarmen/warp]\n脚本特点:\n	* 支持 Warp+ 账户，附带第三方刷 Warp+ 流量和升级内核 BBR 脚本\n	* 普通用户友好的菜单，进阶者通过后缀选项快速搭建\n	* 智能判断vps操作系统：Ubuntu 18.04、Ubuntu 20.04、Debian 10、Debian 11、CentOS 7、CentOS 8，请务必选择 LTS 系统；智能判断硬件结构类型：AMD 或者 ARM\n	* 结合 Linux 版本和虚拟化方式，自动优选三个 WireGuard 方案。网络性能方面：内核集成 WireGuard＞安装内核模块＞boringtun＞wireguard-go\n	* 智能判断 WGCF 作者 github库的最新版本 （Latest release）\n	* 智能分析内网和公网IP生成 WGCF 配置文件\n	* 输出执行结果，提示是否使用 WARP IP ，IP 归属地\n"
 	red "======================================================================================================================\n"
-	green " 脚本版本：$VERSION  功能新增：$TXT\n Version: $VERSION	What's new: $TXT\n 系统信息：							System infomations:\n	当前操作系统：$SYS				Operating System: $SYS\n	内核：$(uname -r)			Kernel: $(uname -r)\n	处理器架构：$ARCHITECTURE					Architecture: $ARCHITECTURE\n	虚拟化：$VIRT						Virtualization: $VIRT "
+	green " 脚本版本：$VERSION  功能新增：$TXT\n Version: $VERSION	What's new: $TXT\n 系统信息：						System infomations:\n	当前操作系统：$SYS				Operating System: $SYS\n	内核：$(uname -r)			Kernel: $(uname -r)\n	处理器架构：$ARCHITECTURE					Architecture: $ARCHITECTURE\n	虚拟化：$VIRT						Virtualization: $VIRT "
 	[[ $TRACE4 = plus ]] && green "	IPv4：$WAN4 ( WARP+ IPv4 ) $COUNTRY4		IPv4：$WAN4 ( WARP+ IPv4 ) $COUNTRY4 "
 	[[ $TRACE4 = on ]] && green "	IPv4：$WAN4 ( WARP IPv4 ) $COUNTRY4		IPv4：$WAN4 ( WARP IPv4 ) $COUNTRY4 "
 	[[ $TRACE4 = off ]] && green "	IPv4：$WAN4 $COUNTRY4			IPv4：$WAN4 $COUNTRY4 "
 	[[ $TRACE6 = plus ]] && green "	IPv6：$WAN6 ( WARP+ IPv6 ) $COUNTRY6	IPv6：$WAN6 ( WARP+ IPv6 ) $COUNTRY6 "
 	[[ $TRACE6 = on ]] && green "	IPv6：$WAN6 ( WARP IPv6 ) $COUNTRY6	IPv6：$WAN6 ( WARP IPv6 ) $COUNTRY6 "
-	[[ $TRACE6 = off ]] && green "	IPv6：$WAN6 $COUNTRY6	IPv6：$WAN6 $COUNTRY6  "
+	[[ $TRACE6 = off ]] && green "	IPv6：$WAN6 $COUNTRY6			IPv6：$WAN6 $COUNTRY6  "
 	[[ $TRACE4 = plus || $TRACE6 = plus ]] && green "	WARP+ 已开启	设备名：$(grep name /etc/wireguard/info.log 2>/dev/null | awk '{ print $NF }')\n	WARP+ is turned on.	Device name：$(grep name /etc/wireguard/info.log 2>/dev/null | awk '{ print $NF }') "
-	[[ $TRACE4 = on || $TRACE6 = on ]] && green "	WARP 已开启	WARP is turned on " 	
-	[[ $TRACE4 = off && $TRACE6 = off ]] && green "	WARP 未开启	WARP is not turned on"
+	[[ $TRACE4 = on || $TRACE6 = on ]] && green "	WARP 已开启								WARP is turned on " 	
+	[[ $TRACE4 = off && $TRACE6 = off ]] && green "	WARP 未开启								WARP is not turned on "
  	red "\n======================================================================================================================\n"
 	}
 
