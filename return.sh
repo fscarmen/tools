8c255c43484d1e800d6a3bd1b17557893347f29d
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-11-07 13:09:42 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -1,9 +1,6 @@
-if [[ $(hostnamectl) =~ .*arm.* ]]
-  then file=besttracearm
-  else file=besttrace
-fi
-
-read -p "当地IP:" ip
-wget -nc https://github.com/fscarmen/tools/raw/main/$file
-chmod +x $file
-./$file $ip -g cn
+[[ ! $(arch) =~ .*x86_64* ]] && echo -e "\033[31m 只支持 AMD64 架构的 VPS 使用，问题反馈:[https://github.com/fscarmen/tools/issues] \033[0m" && exit 1 
+ip=$1
+[[ -z $ip || $ip = '[LOCAL_IP]' ]] && read -p "当地IP:" ip
+wget -nc https://github.com/fscarmen/tools/raw/main/besttrace
+chmod +x besttrace
+./besttrace $ip -g cn
