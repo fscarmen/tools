254d6458239d8f1d2aaa5e077d52cf7ab8c8e9dd
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-01 22:19:34 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -4,31 +4,35 @@
 #Blog https://ty.al
 
 region_area(){
-	area=("$area" "*")
+	region=$(curl --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | sed 's/.*com\/\([^-]\{1,\}\).*/\1/g')
+	region=${region:-US}
+
+	area2=("${area}" "*")
 	display2=("Region: ${region} Done, monitoring..." "Not match, Changing IP...")
 	cmd2=("" "systemctl restart wg-quick@wgcf")
 	sleep_sec2=("6" "3")
-	
-	region=$(curl --user-agent "${UA_Browser}" -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | sed 's/.*com\/\([^-]\{1,\}\).*/\1/g')
-	region=${region:-US}
         
-	for ((i=0; i<${#area[@]}; i++)); do
-		[[ "$region" == "$area[i]" ]] && break
+	for ((j=0; j<${#area2[@]}; j++)); do
+		[[ "$region" == ${area2[j]} ]] && break
 	done
-	echo -e ${display2[i]} && (${cmd2[i]}; sleep ${sleep_sec2[i]})
-        }
+	echo -e ${display2[j]} && (${cmd2[j]}; sleep ${sleep_sec2[j]})
+	}
 
 output=("404" "403" "000" "200")
 display1=("Originals Only, Changing IP..." "No, Changing IP..." "Failed, retrying..." "Matching the region...")
 cmd1=("systemctl restart wg-quick@wgcf" "systemctl restart wg-quick@wgcf" "systemctl restart wg-quick@wgcf" "region_area")
 sleep_sec1=("3" "3" "0" "0")
 
+area=$1
+[[ -n $area ]] && area=$(echo $area | tr '[:upper:]' '[:lower:]')
+
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 read -rp "Is warp installed? [y/n] " input
-[[ "$input" == "n" ]] && bash <(curl -fsSL https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh) || read -rp "Input the region you want(e.g. hk,sg):" area
-area=$(echo $area | tr '[:upper:]' '[:lower:]')
+[[ "$input" == "y" ]] && [[ -z $area ]] && read -rp "Input the region you want(e.g. hk,sg):" area && area=$(echo $area | tr '[:upper:]' '[:lower:]')
+[[ "$input" == "n" ]] && bash <(curl -fsSL https://raw.githubusercontent.com/fscarmen/warp/main/menu.sh)
 
 while [[ "$input" != "n" ]]; do
+	curl ip.gs
 	result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
 		for ((i=0; i<${#output[@]}; i++)); do
 			[[ "$result" == ${output[i]} ]] && break
