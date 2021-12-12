1e955cad573f63e409ce9f6e17e800b01ed93943
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-12 13:04:47 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -10,8 +10,8 @@ aarch64 ) FILE=besttracearm;;
 esac
 
 ip=$1
-green " 本脚说明：测 VPS ——> 本地网络 经过的地区及线路，核心测速程序来由: https://www.ipip.net/ ，请知悉！"
-[[ -z "$ip" || $ip = '[LOCAL_IP]' ]] && reading " 请输入本地网络IP: " ip
-[[ -f "$FILE" ]] || ( green " 下载 IPIP.net 测回程文件 " && wget -N https://github.com/fscarmen/tools/raw/main/besttrace/$FILE >/dev/null 2>&1 )
+green " 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心测速程序来由: https://www.ipip.net/ ，请知悉！"
+[[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading " 请输入目的地 IP: " ip
+[[ -f "$FILE" ]] || ( green " 下载 IPIP.net 测线路文件 " && wget -N https://github.com/fscarmen/tools/raw/main/besttrace/$FILE >/dev/null 2>&1 )
 chmod +x "$FILE" >/dev/null 2>&1
 ./"$FILE" "$ip" -g cn
