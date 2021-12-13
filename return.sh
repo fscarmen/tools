b94a236500ccebf06703954cfe36e4e757819cff
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-13 13:13:04 +0800

    Update return.sh

--- return.sh
+++ return.sh
@@ -7,12 +7,12 @@ case $ARCHITECTURE in
 x86_64 )  FILE=besttrace;;
 aarch64 ) FILE=besttracearm;;
 i386 )    FILE=besttracemac;;
-* ) red " 只支持 AMD64 和 ARM64 架构的 VPS 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1;;
+* ) red " 只支持 AMD64、ARM64、Mac 使用，问题反馈:[https://github.com/fscarmen/tools/issues] " && exit 1;;
 esac
 
 ip=$1
 green " 本脚说明：测 VPS ——> 对端 经过的地区及线路，填本地IP就是测回程，核心程序来由: https://www.ipip.net/ ，请知悉！"
 [[ -z "$ip" || $ip = '[DESTINATION_IP]' ]] && reading " 请输入目的地 IP: " ip
-[[ -f "$FILE" ]] || ( green " 下载 IPIP.net 测线路文件 " && wget -N https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE >/dev/null 2>&1 )
+[[ -f "$FILE" ]] || ( green " 下载 IPIP.net 测线路文件 " && curl -O https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE >/dev/null )
 chmod +x "$FILE" >/dev/null 2>&1
 sudo ./"$FILE" "$ip" -g cn
