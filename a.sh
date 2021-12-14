c334ba95aea7cc61a557b6170cc885c4cb3c7368
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-15 01:23:11 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -945,7 +945,7 @@ menu(){
 		1 )	[[ $OPTION1 = ${T[${L}66]} || $OPTION1 = ${T[${L}67]} ]] && MODIFY=$(eval echo \$MODIFYS$IPV4$IPV6) && install
 			[[ $OPTION1 = ${T[${L}70]} ]] && MODIFY=$(eval echo \$MODIFYD$IPV4$IPV6) && install
 			[[ $OPTION1 = ${T[${L}77]} ]] && onoff;;
-		2 )	[[ $OPTION2 = ${T[${L}68]} || $OPTION2 = ${T[${L}69]} || $OPTION2 = ${T[${L}70]} ]] && MODIFY=$(eval echo \$MODIFYD$IPV4$IPV6) && install
+		2 )	[[ $OPTION2 = ${T[${L}68]} || $OPTION2 = ${T[${L}69]} || $OPTION2 = ${T[${L}34]} ]] && MODIFY=$(eval echo \$MODIFYD$IPV4$IPV6) && install
 			[[ $OPTION2 = ${T[${L}78]} ]] && update;;
 		3 )	[[ $OPTION3 = ${T[${L}71]} ]] && OPTION=o; net
 			[[ $OPTION3 = ${T[${L}123]} ]] && change_ip;;
