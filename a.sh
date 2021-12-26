fd99e0041e8c305f29c96e476a908f1b75846dc9
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-26 13:39:50 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -901,7 +901,7 @@ install(){
 	[[ $CONFIRM = [Yy] ]] && teams_change && echo "$TEAMS" > /etc/wireguard/info.log 2>&1
 
 	# 设置开机启动
-	systemctl enable --now wg-quick@wgcf >/dev/null 2>&1
+	systemctl start wg-quick@wgcf >/dev/null 2>&1
 
 	# 如是 LXC，安装 Wireguard-GO。部分较低内核版本的KVM，即使安装了wireguard-dkms, 仍不能正常工作，兜底使用 wireguard-go
 	[[ $ARCHITECTURE = s390x ]] && S390X='-s390x'
