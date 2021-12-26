2d1cb33fe526f20b259ff259a24d4dd1aca097a1
fscarmen <62703343+fscarmen@users.noreply.github.com>
2021-12-26 22:22:41 +0800

    Update warp_plus.sh

--- warp_plus.sh
+++ warp_plus.sh
@@ -5,13 +5,14 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin && expor
 ### 为啥没在curl请求里面直接填这个变量!?因为不知道啥原因填进去总是错，猜测是-符号的引起的?!
 
 flowdata=${1:-10}
+license=$2
 
 ########
 for((i = 0; i < ${flowdata}; i++)); do
     [[ $i == 0 ]] && sleep_try=30 && sleep_min=20 && sleep_max=600 && echo $(date) Mission $flowdata GB
     install_id=$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 22) && \
     curl -X POST -m 10 -sA "okhttp/3.12.1" -H 'content-type: application/json' -H 'Host: api.cloudflareclient.com' \
-    --data "{\"key\": \"$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 43)=\",\"install_id\": \"$install_id\",\"fcm_token\": \"APA91b$install_id$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 134)\",\"referrer\": \"eb86bd52-fe28-4f03-a944-60428823540e\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(tr -dc '0-9' </dev/urandom | head -c 3)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}" \
+    --data "{\"key\": \"$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 43)=\",\"install_id\": \"$install_id\",\"fcm_token\": \"APA91b$install_id$(tr -dc 'A-Za-z0-9' </dev/urandom | head -c 134)\",\"referrer\": \"$license\",\"warp_enabled\": false,\"tos\": \"$(date -u +%FT%T.$(tr -dc '0-9' </dev/urandom | head -c 3)Z)\",\"type\": \"Android\",\"locale\": \"en_US\"}" \
     --url "https://api.cloudflareclient.com/v0a$(shuf -i 100-999 -n 1)/reg" | grep -qE "referral_count\":1" && status=0 || status=1
     # cloudflare限制了请求频率,目前测试大概在20秒,失败时因延长sleep时间
     [[ $sleep_try > $sleep_max ]] && sleep_try=300
