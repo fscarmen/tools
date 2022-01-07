4cb4f55af0d68dc16cbf8f13fd8be23487ea34a1
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-01-07 17:52:21 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -489,10 +489,8 @@ update(){
 	sed -i "2s#.*#$(sed -ne 2p wgcf-profile.conf)#;3s#.*#$(sed -ne 3p wgcf-profile.conf)#;4s#.*#$(sed -ne 4p wgcf-profile.conf)#" wgcf.conf);;
 	2 ) input_url
 	[[ $CONFIRM = [Yy] ]] && (echo "$TEAMS" > /etc/wireguard/info.log 2>&1
-	teams_change
-	;;
-	* ) red " ${T[${L}51]} [1-2] "; sleep 1; update
-	;;
+	teams_change);;
+	* ) red " ${T[${L}51]} [1-2] "; sleep 1; update;;
 	esac
 	}
 
