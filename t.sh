8cfdd7f4307ae1ada801931bcaa31658adf90b0e
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-01-27 19:13:05 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -174,13 +174,13 @@ warp-cli --accept-tos delete >/dev/null 2>&1 && warp-cli --accept-tos register >
 [[ -e /etc/wireguard/license ]] && warp-cli --accept-tos set-license \$(cat /etc/wireguard/license) >/dev/null 2>&1 && sleep 2; }
 
 check0(){
-i=0; RESULT[i]=""; REGION[i]=""; R[i]="";
-RESULT[i]=\$(curl --user-agent "\${UA_Browser}" $NF -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567"  2>&1)
-if [[ \$RESULT[i] = 200 ]]; then
-REGION[i]=\$(tr '[:lower:]' '[:upper:]' <<< \$(curl --user-agent "${UA_Browser}" $NF -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | sed 's/.*com\/\([^-/]\{1,\}\).*/\1/g'))
-REGION[i]=\${REGION[i]:-'US'}
+RESULT[0]=""; REGION[0]=""; R[0]="";
+RESULT[0]=\$(curl --user-agent "\${UA_Browser}" $NF -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567"  2>&1)
+if [[ \${RESULT[0]} = 200 ]]; then
+REGION[0]=\$(tr '[:lower:]' '[:upper:]' <<< \$(curl --user-agent "${UA_Browser}" $NF -fs --max-time 10 --write-out %{redirect_url} --output /dev/null "https://www.netflix.com/title/80018499" | sed 's/.*com\/\([^-/]\{1,\}\).*/\1/g'))
+REGION[0]=\${REGION[0]:-'US'}
 fi
-echo "\$REGION[i]" | grep -qi "$EXPECT" || R[i]='0'
+echo "\${REGION[0]}" | grep -qi "$EXPECT" || R[i=0]='0'
 }
 
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
