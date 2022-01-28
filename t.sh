004fc45f8bff860f0da80633820ad0c4a27e1348
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-01-28 14:37:00 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -167,11 +167,6 @@ input_streammedia_unlock
 
 [[ -z "$EXPECT" ]] && input_region
 
-select_mode
-
-# 把不同解锁方式做到计划任务里
-sh -c "$TASKS"
-
 # 生成 warp_unlock.sh 文件，判断当前流媒体解锁状态，遇到不解锁时更换 WARP IP，直至刷成功。5分钟后还没有刷成功，将不会重复该进程而浪费系统资源
 cat <<EOF >/etc/wireguard/warp_unlock.sh
 timedatectl set-timezone Asia/Shanghai
