9693f6d6d028e590e72d7d3d97698791589c80ae
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-01-30 13:59:22 +0800

    Update t.sh

--- t.sh
+++ t.sh
@@ -291,10 +291,6 @@ type -P warp-cli >/dev/null 2>&1 && warp-cli --accept-tos register >/dev/null 2>
 green " ${T[${L}11]} "
 }
 
-# 语言转参
-echo "$@" | grep -qwi '\-e' && L=E
-echo "$@" | grep -qwi '\-c' && L=C
-
 # 主程序运行 1/2
 statistics_of_run-times
 select_laguage
