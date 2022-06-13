5d07804f6524357230cdd50e617f58697bd0a03c
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-13 10:11:27 +0800

    Create return_pure.sh

---
+++ return_pure.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+ip=$1
+TEMP_FILE='ip.temp'
+FILE=$(uname -m | sed "s/x86_64/besttrace/" | sed "s/aarch64/besttracearm/")
+
+[[ -z "$ip" ]] && echo -e "请填入上对端 IP" && exit 1
+
+[[ ! -e "$FILE" ]] && wget -qN https://cdn.jsdelivr.net/gh/fscarmen/tools/besttrace/$FILE
+chmod +x "$FILE" >/dev/null 2>&1
+./"$FILE" "$ip" -g cn > $TEMP_FILE
+cat $TEMP_FILE | sed "s/.*\*\(.*\)/\1/g" | sed "s/.*AS[0-9]*//g" | sed "/\*$/d;/^$/d;1d" | uniq | awk '{printf("%d.%s\n"),NR,$0}'
+rm -f $TEMP_FILE
