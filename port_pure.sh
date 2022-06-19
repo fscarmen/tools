64f806f667b59d8054911300ab5496c474bb2e20
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-20 00:13:39 +0800

    Update port_pure.sh

--- port_pure.sh
+++ port_pure.sh
@@ -4,7 +4,8 @@ ip=$1
 [[ -z "$ip" ]] && echo -e "请填入 IPv4:PORT 或者 [IPv6]:PORT" && exit 1
 [[ ! $ip =~ '.' ]] && STACK='6'
 
-CN_35='深圳   阿里云'; CN_40='北京   阿里云'; CN_50='北京   腾讯云'; CN_90='泉州  电信CN2'; CN_103='江苏   电信'; CN_104='江苏   移动'; CN_105='江苏   联通'; CN_150='杭州   阿里云'; CN_160='青岛   阿里云'; CN_210='上海   阿里云'
+NODE_ID=('CN_35' 'CN_40' 'CN_50' 'CN_90' 'CN_103' 'CN_104' 'CN_105' 'CN_150' 'CN_160' 'CN_210')
+NODE_ZH=('深圳\t阿里云 ' '北京\t阿里云 ' '北京\t腾讯云 ' '泉州\t电信CN2' '江苏\t电信 ' '江苏\t移动 ' '江苏\t联通 ' '杭州\t阿里云 ' '青岛\t阿里云 ' '上海\t阿里云 ')
 result0='打开'; result1='关闭'
 
 TOKEN=$(wget -qO- https://tcp$STACK.ping.pe/$ip | grep 'document.cookie' | sed "s/.*document.cookie=\"\([^;]\{1,\}\).*/\1/g")
@@ -16,9 +17,11 @@ until [[ $ALL =~ 'TW_1' ]]; do
   ((j++)) || true
   [[ $j = 5 ]] && break
   ALL=$(wget -qO- --header="cookie: $TOKEN" https://tcp$STACK.ping.pe/ajax_getPingResults_v2.php?stream_id=$STREAM_ID)
-  AREA=($(echo $ALL | python3 -m json.tool | grep CN_ | cut -d \" -f4))
-  RESULT=($(echo $ALL | python3 -m json.tool | grep -A 2 CN_ | grep result | sed "s#[\":, ]##g"))
 done
 
-echo -e "地方   ISP     状态"
-for ((i=0;i<${#AREA[@]};i++)); do echo -e "$(eval echo "\$${AREA[i]}")     $(eval echo "\$${RESULT[i]}")"; done
+echo -e "地方\tISP\t\t状态"
+for ((i=0;i<${#NODE_ID[@]};i++)); do
+  RESULT[i]=$(echo $ALL | python3 -m json.tool | grep -A 2 ${NODE_ID[i]} | grep result | sed "s#[\":, ]##g")
+  RESULT[i]=${RESULT[i]:-'result1'}
+  echo -e "${NODE_ZH[i]}\t\t$(eval echo "\$${RESULT[i]}")"
+done
