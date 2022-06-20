6c04e2296111654ecb98e614c444d5471b041e66
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-20 14:27:36 +0800

    Update port_pure.sh

--- port_pure.sh
+++ port_pure.sh
@@ -19,7 +19,7 @@ until [[ $ALL =~ 'TW_1' ]]; do
   ALL=$(wget -qO- --header="cookie: $TOKEN" https://tcp$STACK.ping.pe/ajax_getPingResults_v2.php?stream_id=$STREAM_ID)
 done
 
-printf "%-10s %-10s %-10s\n" ' ' 地方 ISP
+printf "%-10s %-10s %-10s\n" 状态 地方 ISP
 for ((i=0;i<${#NODE_ID[@]};i++)); do
   RESULT[i]=$(echo $ALL | python3 -m json.tool | grep -A 2 ${NODE_ID[i]} | grep result | sed "s#[\":, ]##g")
   RESULT[i]=${RESULT[i]:-'result1'}
