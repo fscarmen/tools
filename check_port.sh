fb2a7a92b5ec0b923667f3c04b48c796ff7caba3
fscarmen <62703343+fscarmen@users.noreply.github.com>
2022-06-23 16:54:35 +0800

    Update check_port.sh

--- check_port.sh
+++ check_port.sh
@@ -14,15 +14,17 @@ NODE_ID=('CN_35' 'CN_40' 'CN_50' 'CN_90' 'CN_103' 'CN_104' 'CN_105' 'CN_150' 'CN
 NODE_ZH=('深圳\t 阿里云' '北京\t 阿里云' '北京\t 腾讯云' '泉州\t 电信CN2' '江苏\t 电信' '江苏\t 移动' '江苏\t 联通' '杭州\t 阿里云' '青岛\t 阿里云' '上海\t 阿里云')
 result0='√'; result1='X'
 
-TOKEN=$(wget -qO- https://tcp$STACK.ping.pe/$ip | grep 'document.cookie' | sed "s/.*document.cookie=\"\([^;]\{1,\}\).*/\1/g")
-STREAM_ID=$(wget -qO- --header="cookie: $TOKEN" https://tcp$STACK.ping.pe/$ip | grep 'stream_id =' | cut -d \' -f2)
+TOKEN=$(wget -t1 -T5 -qO- https://tcp$STACK.ping.pe/$ip | grep 'document.cookie' | sed "s/.*document.cookie=\"\([^;]\{1,\}\).*/\1/g")
+[ -z "$TOKEN" ] && red " 不能正常访问 ping.pe，请稍后再试，脚本退出。\n" && exit 1
+STREAM_ID=$(wget -t1 -T5 -qO- --header="cookie: $TOKEN" https://tcp$STACK.ping.pe/$ip | grep 'stream_id =' | cut -d \' -f2)
 sleep 3
 until [[ $ALL =~ 'TW_1' ]]; do
   sleep 2
   ((j++)) || true
   [[ $j = 5 ]] && break
-  ALL=$(wget -qO- --header="cookie: $TOKEN" https://tcp$STACK.ping.pe/ajax_getPingResults_v2.php?stream_id=$STREAM_ID)
+  ALL=$(wget -t1 -T5 -qO- --header="cookie: $TOKEN" https://tcp$STACK.ping.pe/ajax_getPingResults_v2.php?stream_id=$STREAM_ID)
 done
+[ -z "$ALL" ] && red " 获取不了数据，请输入正确的格式: IPv4:port, [IPv6]:port, 域名:port\n" && exit 1
 
 yellow " 状态\t 地方\t ISP "
 for ((i=0;i<${#NODE_ID[@]};i++)); do
