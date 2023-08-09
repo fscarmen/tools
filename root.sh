f1bee55ce554688cd28df26c60514cb519c37c9f
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-09 20:43:07 +0800

    补全修改，避免部分机器无法设置 (#2)
    来自
    
    https://github.com/spiritLHLS/pve/blob/main/scripts/ssh.sh
    
    对应本脚本的优化

--- root.sh
+++ root.sh
@@ -3,5 +3,19 @@ password=$1
 [[ -z $password || $password = '[PASSWORD]' ]] && read -p "请输入root密码:" password
 echo root:$password | chpasswd root
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g;s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config
+sed -i 's/#ListenAddress ::/ListenAddress ::/' /etc/ssh/sshd_config
+sed -i 's/#AddressFamily any/AddressFamily any/' /etc/ssh/sshd_config
+sed -i '/^#UsePAM\|UsePAM/c #UsePAM no' /etc/ssh/sshd_config
+sed -i 's/^#\?PubkeyAuthentication.*/PubkeyAuthentication no/g' /etc/ssh/sshd_config
+sed -i '/^AuthorizedKeysFile/s/^/#/' /etc/ssh/sshd_config
+if [ -f /etc/ssh/sshd_config.d/50-cloud-init.conf ]; then
+    sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config.d/50-cloud-init.conf
+    sed -i 's/#ListenAddress ::/ListenAddress ::/' /etc/ssh/sshd_config.d/50-cloud-init.conf
+    sed -i 's/#AddressFamily any/AddressFamily any/' /etc/ssh/sshd_config.d/50-cloud-init.conf
+    sed -i '/^#UsePAM\|UsePAM/c #UsePAM no' /etc/ssh/sshd_config.d/50-cloud-init.conf
+    sed -i "s/^#\?PubkeyAuthentication.*/PubkeyAuthentication no/g" /etc/ssh/sshd_config.d/50-cloud-init.conf
+    sed -i '/^AuthorizedKeysFile/s/^/#/' /etc/ssh/sshd_config.d/50-cloud-init.conf
+fi
 service sshd restart
 echo -e "\033[32m 请重新登陆，用户名：root ， 密码：$password \033[0m"
