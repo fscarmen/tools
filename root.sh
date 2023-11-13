bb22f76d1e1b84be4a9d88c5b550b287e8132ae6
fscarmen <62703343+fscarmen@users.noreply.github.com>
2023-11-13 10:04:44 +0800

    增加踢其他 ssh 用户下线的功能

--- root.sh
+++ root.sh
@@ -1,7 +1,16 @@
 [[ $(id -u) != 0 ]] && echo -e "\033[31m 必须以root方式运行脚本，可以输入 sudo -i 后重新下载运行，问题反馈:[https://github.com/fscarmen/tools/issues] \033[0m" && exit 1
 password=$1
 [[ -z $password || $password = '[PASSWORD]' ]] && read -p "请输入root密码:" password
-echo root:$password | chpasswd root
+
+# 把其他 ssh 登陆用户踢下线
+current_tty=$(tty); 
+pts_list=$(who | awk '{print $2}')
+for pts in $pts_list; do
+  [ "$current_tty" != "/dev/$pts" ] && pkill -9 -t $pts
+done
+
+# 修改密码
+echo "root:$password" | chpasswd
 sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g;s/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
 sed -i 's/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config
 sed -i 's/#ListenAddress ::/ListenAddress ::/' /etc/ssh/sshd_config
@@ -17,5 +26,14 @@ if [ -f /etc/ssh/sshd_config.d/50-cloud-init.conf ]; then
     sed -i "s/^#\?PubkeyAuthentication.*/PubkeyAuthentication no/g" /etc/ssh/sshd_config.d/50-cloud-init.conf
     sed -i '/^AuthorizedKeysFile/s/^/#/' /etc/ssh/sshd_config.d/50-cloud-init.conf
 fi
+
+# 再次把其他 ssh 登陆用户踢下线
 service sshd restart
+current_tty=$(tty); 
+pts_list=$(who | awk '{print $2}')
+for pts in $pts_list; do
+  [ "$current_tty" != "/dev/$pts" ] && pkill -9 -t $pts
+done
+
+# 显示结果
 echo -e "\033[32m 请重新登陆，用户名：root ， 密码：$password \033[0m"
