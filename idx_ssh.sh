e60419422a2baa64f3153c793643350d978e8517
fscarmen <62703343+fscarmen@users.noreply.github.com>
2025-05-02 09:09:09 +0800

    Update idx_ssh.sh

--- idx_ssh.sh
+++ idx_ssh.sh
@@ -48,10 +48,10 @@ echo -e "${YELLOW}[4/5] 正在设置 root 用户密码...${RESET}"
 echo root:$PASSWORD | chpasswd root
 
 echo -e "${YELLOW}[5/5] 正在解除 SSH 和 Docker 服务的锁定，启用密码访问...${RESET}"
-systemctl unmask ssh docker containerd docker.socket 
+systemctl unmask ssh containerd docker.socket docker
 pkill dockerd
 pkill containerd
-systemctl start ssh docker containerd docker.socket 
+systemctl start ssh containerd docker.socket docker
 
 # 下载并设置 ngrok
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz -qO- | tar -xz -C /usr/local/bin
